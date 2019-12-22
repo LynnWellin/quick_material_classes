dd5efcaaa64934f137467bf571db9c80e371533c
Yury Lebedev <y.d.lebedev@gmail.com>
2019-12-22 10:09:59 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,19 @@
+export default classes = {
+    absoluteCover: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0
+    },
+    flexCenter: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+}
+
+export default palette = {
+    black: '#000000',
+    white: '#ffffff',
+}
